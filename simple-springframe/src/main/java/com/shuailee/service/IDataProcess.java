0f9984527fb7cadcdb30a37797738a9b83358c78
lishuai <lishuai15@glcp.com.cn>
2020-12-21 18:40:11 +0800

    init

--- simple-springframe/src/main/java/com/shuailee/service/IDataProcess.java
+++
@@ -1,6 +0,0 @@
-package com.shuailee.service;
-
-public interface IDataProcess {
-    String getName(String name);
-    String getAge(Integer age);
-}
--- ssm(spring+servlet+mybatis)/README.md
+++
@@ -1,2 +0,0 @@
-# ssm
-spring + servlet + mybatis
