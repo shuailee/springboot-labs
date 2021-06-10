f76f19cda0542ffe1da5c92ad4be0c18411d5e86
lishuai <lishuai15@glcp.com.cn>
2021-06-10 11:06:52 +0800

    mybatisplus 分页拦截器配置

---
+++ lab-01-springboot-hub/src/main/java/com/klein/config/MyBatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.klein.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @package: com.klein.config
+ * @description: 分页拦截器配置  3.4之前使用PaginationInterceptor  3.4之后使用MybatisPlusInterceptor
+ * @author: klein
+ * @date: 2021-06-10 10:36
+ **/
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setOverflow(false);
+        paginationInnerInterceptor.setMaxLimit(500L);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+}
--- lab-01-springboot-hub/src/test/java/com/klein/dao/mapper/UserMapperTest.java
+++ lab-01-springboot-hub/src/test/java/com/klein/dao/mapper/UserMapperTest.java
@@ -1,5 +1,7 @@
 package com.klein.dao.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.klein.Lab01SpringbootHubApplicationTests;
 import com.klein.dao.model.User;
 import org.junit.Assert;
@@ -9,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -24,11 +26,54 @@ public class UserMapperTest {
 
     @Autowired
     UserMapper userMapper;
+
     @Test
-    public void testQuery() {
+    public void testSelectList() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
         Assert.assertEquals(5, userList.size());
         userList.forEach(System.out::println);
     }
+
+    @Test
+    public void testSelectByMap() {
+        System.out.println(("----- selectByMap method test ------"));
+        Map<String, Object> columnMap = new HashMap<>();
+        columnMap.put("id", "1");//写表中的列名
+        List<User> userList = userMapper.selectByMap(columnMap);
+        Assert.assertEquals(1, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectBatchIds() {
+        System.out.println(("----- selectBatchIds method test ------"));
+        List<Integer> idList = Arrays.asList(1, 2, 3);
+        List<User> userList = userMapper.selectBatchIds(idList);
+        userList.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testSelectPage() {
+
+        System.out.println(("----- selectList method process ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+
+        System.out.println(("----- selectPage method test ------"));
+
+        // 内存分页
+        // Page<User> userPage = userMapper.selectPage(new Page<>(1, 2), null);
+        // 使用QueryWrapper条件构造器进行分页，物理分页
+        Page<User> userPage = userMapper.selectPage(new Page<User>(1, 2),
+                new QueryWrapper<User>()
+                        .between("age", 18, 50)
+                        .in("id", Arrays.asList(1, 2, 3, 4, 5))
+        );
+
+        List<User> userPageList = userPage.getRecords();
+        userPageList.forEach(System.out::println);
+    }
+
 }
